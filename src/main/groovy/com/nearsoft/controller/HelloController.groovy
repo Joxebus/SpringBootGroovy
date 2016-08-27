3a9ce04d91092880132fd500a8c9fe3dc598621f
Omar Bautista <obautista@skytouchtechnology.com>
2016-08-27 16:23:14 -0700

    Logging configuration
    The configuration to enable logging has been added:
    
    - Slf4j 1.7.15
    - Logback 1.1.7
    
    If you use different version of this may be you will have an exception like this:
    
    	Exception in thread "main" java.lang.NoClassDefFoundError: org/slf4j/event/LoggingEvent
    
    To see more information you can go to the official site: http://www.slf4j.org/codes.html
    
    The `log-config.xml` contains the configuration of the loggin levels, also see the `application.yml` file to see how to configure logging for the app.

--- src/main/groovy/com/nearsoft/controller/HelloController.groovy
+++ src/main/groovy/com/nearsoft/controller/HelloController.groovy
@@ -1,19 +1,25 @@
 package com.nearsoft.controller
 
+import groovy.util.logging.Slf4j
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 import javax.annotation.PostConstruct
 
 @RestController
+@Slf4j
 class HelloController {
 
     @PostConstruct
     def init(){
-        println 'Controller generated'
+        log.info 'Controller generated'
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/",
+        method = RequestMethod.POST,
+            consumes = "application/json",
+            produces = "application/json")
     def index(){
         "Hello World!"
     }
